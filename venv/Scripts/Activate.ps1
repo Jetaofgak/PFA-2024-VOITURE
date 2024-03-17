b7d9606214b12b83d1b333ca52a2b62d9d4d7620
Jetaofgak <dahamitaoufik@gmail.com>
2024-03-17 01:20:37 +0000

    Venv commiting

---
+++ venv/Scripts/Activate.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b736e71c45e23b3ab93d5a5d91bab9514846f2aed74cc5d71f2273a03b5d878
+size 21976
---
+++ venv/Scripts/activate.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:639366ca2e39ec25f1c2017d67fe36d60d52b73d68e1e6c106326a4179870fb2
+size 980
